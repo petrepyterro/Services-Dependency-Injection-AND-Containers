530ae2c4d64536add1fe3a3fdd992da6525719d0
petrero <petrero@gmail.com>
2017-03-19 19:16:57 +0200

    Copy code from previous branch

---
+++ lib/ShipLoader.php
@@ -0,0 +1,59 @@
+<?php
+
+class ShipLoader{
+  /**
+   * 
+   * @return Ship[]
+   */
+  function getShips(){
+    $shipsData = $this->queryForShips();
+    
+    $ships = array();
+    
+    foreach ($shipsData as $shipData){  
+      $ships[] = $this->createShipFromData($shipData);
+    }
+    
+    return $ships;
+  }
+  
+  /*
+   * @param $id
+   * @return Ship
+   */
+  public function findOneById($id) {
+    $pdo = new PDO('mysql:host=localhost;dbname=Services_Dependency_Injection_AND_Containers','root', 'mysql');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $statement = $pdo->prepare('SELECT * FROM ship WHERE id = :id');
+    $statement->execute(array('id' => $id));
+    $shipArray = $statement->fetch(PDO::FETCH_ASSOC);
+    
+    if(!$shipArray){
+      return null;
+    }
+    
+    return $this->createShipFromData($shipArray);
+  }
+  
+  private function queryForShips(){
+    $pdo = new PDO('mysql:host=localhost;dbname=Services_Dependency_Injection_AND_Containers','root', 'mysql');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $statement = $pdo->prepare('SELECT * FROM ship');
+    $statement->execute();
+    $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+    return $shipsArray;
+  }
+  
+  private function createShipFromData(array $shipData) {
+    $ship = new Ship($shipData['name']);
+    $ship->setId($shipData['id']);
+    $ship->setWeaponPower($shipData['weapon_power']);
+    $ship->setJediFactor($shipData['jedi_factor']);
+    $ship->setStrength($shipData['strength']);
+    
+    return $ship;
+  }
+  
+}
+
