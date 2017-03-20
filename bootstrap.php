4f7ea041052df0656baccbfcbbd8410e20fc551b
petrero <petrero@gmail.com>
2017-03-20 21:30:50 +0200

    Copy code from previous branch

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__.'/lib/Ship.php';
+require_once __DIR__.'/lib/BattleManager.php';
+require_once __DIR__.'/lib/BattleResult.php';
+require_once __DIR__.'/lib/ShipLoader.php';
+require_once __DIR__.'/lib/Container.php';
+
+
+$configuration = array(
+  'db_dsn'  => 'mysql:host=localhost;dbname=Services_Dependency_Injection_AND_Containers',
+  'db_user' => 'root',
+  'db_pass' => 'mysql'  
+);
+
